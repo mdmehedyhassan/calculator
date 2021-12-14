172e00f78e5f2163a738bf2ba8371d9797410c14
unknown <mdmehedyh55@gamil.com>
2021-12-14 19:47:27 +0600

    add new

--- script.js
+++ script.js
@@ -19,12 +19,17 @@ function mathEquation(equation){
     const numberInput = document.getElementById('math-display').innerText;
     const lastChar = numberInput.charAt(numberInput.length - 1);
     let removeLast = numberInput;
-    if (lastChar == '' || lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/'){
+    if (lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/'){
         removeLast = numberInput.slice(0, -1)
-        console.log(lastChar);
     }
+
     const newNumber = removeLast + equation;
     document.getElementById('math-display').innerText = newNumber;
+
+    if(lastChar == ''){
+        document.getElementById('math-display').innerText = '';
+    }
+
 }
 
 function mathEqual(id){
@@ -47,17 +52,3 @@ function mathEqual(id){
         document.getElementById('math-solution').innerText = x + '.' + y;
     }
 }
-
-// function mathSqrt(){
-//     const totalNumberInput = document.getElementById('math-display').innerText;
-//     const totalNumber = Math.sqrt(totalNumberInput);
-//     const totalNumberString = totalNumber + '';
-//     let [x, y] = totalNumberString.split('.');
-//     if(y == undefined){
-//         document.getElementById('math-solution').innerText = x ;
-//     }
-//     if(y != undefined){
-//         y = y.slice(0, 2);
-//         document.getElementById('math-solution').innerText = x + '.' + y;
-//     }
-// }
